5185e6d4c6954f2e03bcc5422a401bb6ffca10f5
Puszkarski <barpus@dsic.upv.es>
2020-03-27 12:49:07 +0100

    available login function and booking management

--- PaddleExperience/src/paddleexperience/FXMLDocumentController.java
+++ PaddleExperience/src/paddleexperience/FXMLDocumentController.java
@@ -23,48 +23,71 @@ import paddleexperience.CustomExceptions.SignupException;
  * @author barpus
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
     private Label label;
     private ClubDBAccess clubDB;
-    
+    private Member member;
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText(clubDB.getClubName());
-        try{
-            signUp("Bartosz", "Puszkarski" , "12312312312", "Login with blank", "PasswordWithB@@@@D Chars", "", "", null);
-        } catch (SignupException e){
+        try {
+            signUp("Bartosz", "Puszkarski", "12312312312", "Login with blank", "PasswordWithB@@@@D Chars", "", "", null);
+        } catch (SignupException e) {
             System.out.print(e.getMessage());
         }
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        clubDB = ClubDBAccess.getSingletonClubDBAccess();
-        
-    }  
+        this.clubDB = ClubDBAccess.getSingletonClubDBAccess();
+
+    }
     
     
-    private void signUp(String name, String surname, String phone, String login, String password, String creditCard, String svc, Image image) throws SignupException{
-        
+    private boolean login(String login, String password){
+        Member loggedMember = clubDB.getMemberByCredentials(login, password);
+        if(loggedMember == null){
+            return false;
+        } 
+        this.member = loggedMember;
+        return true;
+    }
+
+    private boolean signUp(String name, String surname, String phone, String login, String password, String creditCard, String svc, Image image) throws SignupException {
+
         ArrayList<String> errors = new ArrayList<>();
-        
-        if(name.isEmpty()) errors.add("Invalid Name");
-        if(surname.isEmpty()) errors.add("Invalid Surname");
-        if(phone.isEmpty()) errors.add("Invalid Phone Number");
-        if(!validLogin(login)) errors.add("Invalid Login");
-        if(!validPassword(password)) errors.add("Invalid Password");
-        if(!validCreditCard(creditCard, svc)) errors.add("Invalid Credit Card Number or SVC");
-        
-        if(errors.isEmpty()) {
-            clubDB.getMembers().add(new Member(name, surname, phone, login, password, creditCard, svc, image)); 
+
+        if (name.isEmpty()) {
+            errors.add("Invalid Name");
+        }
+        if (surname.isEmpty()) {
+            errors.add("Invalid Surname");
+        }
+        if (phone.isEmpty()) {
+            errors.add("Invalid Phone Number");
+        }
+        if (!validLogin(login)) {
+            errors.add("Invalid Login");
+        }
+        if (!validPassword(password)) {
+            errors.add("Invalid Password");
+        }
+        if (!validCreditCard(creditCard, svc)) {
+            errors.add("Invalid Credit Card Number or SVC");
+        }
+
+        if (errors.isEmpty()) {
+            clubDB.getMembers().add(new Member(name, surname, phone, login, password, creditCard, svc, image));
+            return true;
         }
         throw new SignupException(String.join(", ", errors));
     }
 
     private boolean validLogin(String login) {
-          return  !login.contains(" ") && !clubDB.existsLogin(login); 
+        return !login.contains(" ") && !clubDB.existsLogin(login);
     }
 
     private boolean validPassword(String password) {
@@ -72,10 +95,10 @@ public class FXMLDocumentController implements Initializable {
     }
 
     private boolean validCreditCard(String creditCard, String svc) {
-        return (creditCard.isEmpty() && svc.isEmpty()) || 
-                    (creditCard.chars().allMatch(Character::isDigit) &&
-                    svc.chars().allMatch(Character::isDigit) && 
-                    creditCard.length() == 16 &&
-                    svc.length() == 3) ;
+        return (creditCard.isEmpty() && svc.isEmpty())
+                || (creditCard.chars().allMatch(Character::isDigit)
+                && svc.chars().allMatch(Character::isDigit)
+                && creditCard.length() == 16
+                && svc.length() == 3);
     }
 }
