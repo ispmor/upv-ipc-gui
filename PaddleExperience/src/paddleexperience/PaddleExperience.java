827dc2dc2462609417abf0dfeb3bc6bc1cee3691
Christian Cabrera Miñano <62292494+ChrisCaMi78@users.noreply.github.com>
2020-03-30 16:09:09 +0200

    Add files via upload

--- PaddleExperience/src/paddleexperience/PaddleExperience.java
+++ PaddleExperience/src/paddleexperience/PaddleExperience.java
@@ -5,10 +5,15 @@
  */
 package paddleexperience;
 
+import java.io.IOException;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -18,13 +23,11 @@ import javafx.stage.Stage;
 public class PaddleExperience extends Application {
     
     @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("PaddleExperienceView.fxml"));
+        primaryStage.setTitle("Inicio");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 
     /**
---
+++ PaddleExperience/src/paddleexperience/RegisterController.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paddleexperience;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+
+
+
+
+/**
+ *
+ * @author Chris
+ */
+public class RegisterController implements Initializable {
+    
+    
+    @FXML
+    private void confirm(ActionEvent event) throws IOException {
+
+        
+        
+    }
+    
+    @FXML
+    private void cancel(ActionEvent event) throws IOException {
+    
+        
+        
+    }
+   
+    
+     
+    
+        
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+        
+    
+    }
+
+}
